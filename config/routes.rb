39ae2cc55e9990bf95f80db4c17e2796f4c047c7
EC2 Default User <hayato.sa9211@gmail.com>
2021-12-12 16:31:44 +0000

    [Add]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,39 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
-  resource :users
-  resources :products, only: [:index, :show]
-  # get 'meals/index'
-  # get 'meals/new'
-  # get 'meals/create'
-  # get 'meals/show'
-  # get 'meals/edit'
-  # get 'meals/update'
-  # get 'meals/destroy'
-  # get 'bodys/index'
-  # get 'bodys/new'
-  # get 'bodys/create'
-  # get 'bodys/show'
-  # get 'bodys/edit'
-  # get 'bodys/update'
-  # get 'bodys/destroy'
-  # get 'events/index'
-  # get 'events/new'
-  # get 'events/create'
-  # get 'events/edit'
-  # get 'events/update'
-  # get 'events/destroy'
-  # get 'trainings/index'
-  # get 'trainings/new'
-  # get 'trainings/create'
-  # get 'trainings/show'
-  # get 'trainings/edit'
-  # get 'trainings/update'
-  # get 'trainings/destroy'
-  # get 'users/show'
-  # get 'users/edit'
-  # get 'users/update'
-  # get 'users/unsubscribe'
-  # get 'users/withdraw'
+  resource :users, only: [:show, :edit, :update] do
+    collection do
+      get "unsubscribe"
+      patch "withdraw"
+    end
+  end
+  resources :trainings, :bodys, :meals
+  resources :events, except: [:show]
 
 end
