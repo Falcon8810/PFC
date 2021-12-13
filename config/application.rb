f1bec42e27baaad395f679e2951419747988dbd8
EC2 Default User <hayato.sa9211@gmail.com>
2021-12-13 10:21:15 +0000

    [Add]enum日本語

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module PFC
 
     #デフォルトの言語を日本語(:ja)にする
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
