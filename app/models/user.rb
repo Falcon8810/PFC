ebe1f3158ff832ffeb8f19f1599a7bbad1134bcf
EC2 Default User <hayato.sa9211@gmail.com>
2021-12-14 22:27:04 +0000

    [Add]Partial template header

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+  # validates :sex, presence: true
+  validates :birthday, presence: true
+  validates :height, presence: true
+  validates :weight, presence: true
+
   #refileが特定のカラムにアクセスするための記述
   attachment :profile_image
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
-  resource :users, only: [:show, :edit, :update] do
+  get 'my_page' => 'users#show', as: 'my_page'
+  resource :users, only: [:edit, :update] do
     collection do
       get "unsubscribe"
       patch "withdraw"
