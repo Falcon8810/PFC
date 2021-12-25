9b0c404ba2d223c1c082df5449e54762104a519a
Falcon8810 <hayato.sa9211@gmail.com>
2021-12-25 10:28:56 +0000

    [Add]calc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @training_details = TrainingDetail.all
+    
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,5 +33,59 @@ class User < ApplicationRecord
   enum activity_level: { low: 0, middle: 1, high: 2 }
   enum purpose: { diet: 0, keep: 1, bulkup: 2 }
 
+# ユーザーの年齢を計算するメソッド
+  def age
+    d1=self.birthday.strftime("%Y%m%d").to_i
+    d2=Date.today.strftime("%Y%m%d").to_i
+    return (d2 - d1) / 10000
+  end
+
+  # 基礎代謝計算式
+  # The Basal Metabolic Rate (BMR)
+  def calc_bmr
+    if sex == true
+      (10 * weight) + (6.25 * height) - (5 * age) + 5
+    else
+      (10 * weight) + (6.25 * height) - (5 * age) - 161
+    end
+  end
+
+  # 一日あたりの活動代謝
+  def calc_activity_metabolism
+    if activity_level == 0
+      calc_bmr * 1.2
+    elsif activity_level == 1
+      calc_bmr * 1.55
+    else
+      calc_bmr * 1.725
+    end
+  end
+
+  # 目的に合わせたカロリー設定
+  def by_purpose
+    if purpose == 0
+      calc_activity_metabolism * 0.8
+    elsif purpose == 1
+      calc_activity_metabolism
+    else
+      calc_activity_metabolism * 1.2
+    end
+  end
+
+  # 一日当たりのタンパク質（グラム表示）
+  def protein_per_day
+    weight * 2
+  end
+
+# 一日あたりの脂質（グラム表示）
+  def fat_per_day
+    by_purpose * 0.25 / 9
+  end
+
+# 一日あたりの炭水化物（グラム表示）
+  def carb_per_day
+    (by_purpose - (protein_per_day * 4 + by_purpose * 0.25)) / 4
+  end
+
 
 end
